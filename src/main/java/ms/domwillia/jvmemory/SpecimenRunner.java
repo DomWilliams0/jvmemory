f018e91eb09c20ddb15bfed6eb0e42c79f76d293
Dom Williams <dxw405@cs.bham.ac.uk>
2017-10-26 17:46:23 +0100

    Fix putfield uninitialisedThis errors in constructors

--- src/main/java/ms/domwillia/jvmemory/SpecimenRunner.java
+++ src/main/java/ms/domwillia/jvmemory/SpecimenRunner.java
@@ -1,6 +1,6 @@
 package ms.domwillia.jvmemory;
 
-import ms.domwillia.jvmemory.specimen.ManyDups;
+import ms.domwillia.jvmemory.specimen.LocalVars;
 import ms.domwillia.jvmemory.specimen.Specimen;
 
 public class SpecimenRunner {
@@ -8,7 +8,7 @@ public class SpecimenRunner {
 	public static void main(String[] args) {
 		Specimen specimen;
 
-		specimen = new ManyDups();
+		specimen = new LocalVars();
 
 		specimen.go();
 
