a8aedcdc0f54bc4a5a0e9fa8a73903752cc885be
Dom Williams <dxw405@cs.bham.ac.uk>
2017-10-27 17:23:14 +0100

    Add getfield support

--- src/main/java/ms/domwillia/jvmemory/SpecimenRunner.java
+++ src/main/java/ms/domwillia/jvmemory/SpecimenRunner.java
@@ -1,6 +1,6 @@
 package ms.domwillia.jvmemory;
 
-import ms.domwillia.jvmemory.specimen.LocalVars;
+import ms.domwillia.jvmemory.specimen.FieldManipulation;
 import ms.domwillia.jvmemory.specimen.Specimen;
 
 public class SpecimenRunner {
@@ -8,7 +8,7 @@ public class SpecimenRunner {
 	public static void main(String[] args) {
 		Specimen specimen;
 
-		specimen = new LocalVars();
+		specimen = new FieldManipulation();
 
 		specimen.go();
 
