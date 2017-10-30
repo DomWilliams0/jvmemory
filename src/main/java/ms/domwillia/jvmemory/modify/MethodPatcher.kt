280fa7a09863d30a33d4217d2319b69cb7f67ee5
Dom Williams <dxw405@cs.bham.ac.uk>
2017-10-30 14:17:05 +0000

    Add replacement but currently empty method patcher

---
+++ src/main/java/ms/domwillia/jvmemory/modify/MethodPatcher.kt
@@ -0,0 +1,22 @@
+package ms.domwillia.jvmemory.modify
+
+import ms.domwillia.jvmemory.monitor.LocalVarTracker
+import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.commons.InstructionAdapter
+import org.objectweb.asm.commons.LocalVariablesSorter
+
+// TODO helper data class for class+method
+class MethodPatcher(
+        delegate: MethodVisitor,
+        private val className: String,
+        private val methodName: String,
+        private val localVarTracker: LocalVarTracker
+) : InstructionAdapter(Opcodes.ASM6, delegate) {
+
+    lateinit var localVarSorter: LocalVariablesSorter
+
+    private val isConstructor
+        get() = this.methodName == "<init>"
+
+}
