575377c0b011f8b91e1b05a2b8d70912c37ceec8
Dom Williams <dxw405@cs.bham.ac.uk>
2017-10-30 13:38:27 +0000

    Convert StackTracker to Kotlin and remove staticness

---
+++ src/main/java/ms/domwillia/jvmemory/monitor/StackTracker.kt
@@ -0,0 +1,23 @@
+package ms.domwillia.jvmemory.monitor
+
+import java.util.*
+
+class StackTracker {
+    private val stack = Stack<Frame>()
+
+    val head: String
+        get() = stack.peek().let { (clazz, method) -> "${prettyClass(clazz)}:$method" }
+
+
+    fun push(clazz: String, method: String) {
+        stack.push(Frame(clazz, method))
+    }
+
+    fun pop() {
+        stack.pop()
+    }
+
+    private fun prettyClass(clazz: String): String = clazz.substring(clazz.lastIndexOf('/') + 1)
+
+    internal data class Frame(val clazz: String, val method: String)
+}
