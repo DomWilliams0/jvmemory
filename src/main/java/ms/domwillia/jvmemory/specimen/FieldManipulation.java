3b2fcfd05c223b2ad7b55e7ae8e91403adcd934e
Dom Williams <dxw405@cs.bham.ac.uk>
2017-11-02 09:58:42 +0000

    Implement putfield tracking

--- src/main/java/ms/domwillia/jvmemory/specimen/FieldManipulation.java
+++ src/main/java/ms/domwillia/jvmemory/specimen/FieldManipulation.java
@@ -3,15 +3,29 @@ package ms.domwillia.jvmemory.specimen;
 public class FieldManipulation implements Specimen {
 
 	static int staticI = 0;
+	static double staticD = 0;
+	static String staticS = "hiya";
+
 	int i = 0;
+	double d = 0;
 	String s = "hiya";
 
 	@Override
 	public void go() {
-		this.s = "potato";
-		String x = s;
+		int a = i;
+		double b = d;
+		String c = s;
 
-		staticI = 40;
-		int s = staticI;
+		i = 10;
+		d = 40.0;
+		s = "hiya";
+
+//		int e = staticI;
+//		double f = staticD;
+//		String g = staticS;
+//
+//		staticI = 10;
+//		staticD = 40.0;
+//		staticS = "hiya";
 	}
 }
