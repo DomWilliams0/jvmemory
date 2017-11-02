318bc78aa74d3f3f26d1f9c298dc786ff854b188
Dom Williams <dxw405@cs.bham.ac.uk>
2017-11-02 10:30:34 +0000

    Add local var declaration tracking

--- src/main/java/ms/domwillia/jvmemory/monitor/LocalVarTracker.kt
+++ src/main/java/ms/domwillia/jvmemory/monitor/LocalVarTracker.kt
@@ -6,17 +6,17 @@ class LocalVarTracker {
     // method name -> var
     private val vars = HashMap<String, MutableList<LocalVar>>()
 
-    fun registerLocalVar(method: String, name: String, index: Int) {
+    fun registerLocalVar(method: String, name: String, type: String, index: Int) {
         val locals = vars.getOrPut(method, ::mutableListOf)
-        locals.add(LocalVar(index, name))
+        locals.add(LocalVar(index, name, type))
     }
 
     fun debugPrint() {
         vars.forEach { (key, value) ->
             println("$key:")
-            value.forEach { println("\t${it.index} - ${it.name}") }
+            value.forEach { println("\t${it.index} - ${it.type} ${it.name}") }
         }
     }
 
-    private data class LocalVar(val index: Int, val name: String)
+    private data class LocalVar(val index: Int, val name: String, val type: String)
 }
