ec56b103962b1bf96be7ec06b0f992137e6f7bcc
Dom Williams <dxw405@cs.bham.ac.uk>
2017-11-03 17:31:31 +0000

    Add protobuf class definitions

--- src/main/java/ms/domwillia/jvmemory/ProtoBufTester.java
+++ src/main/java/ms/domwillia/jvmemory/ProtoBufTester.java
@@ -3,8 +3,6 @@ package ms.domwillia.jvmemory;
 public class ProtoBufTester {
 
 	public static void main(String[] args) {
-		MemoryAccess.Test.newBuilder()
-				.setMessage("hiya")
-				.build();
+		Definitions.ClassDefinition def;
 	}
 }
