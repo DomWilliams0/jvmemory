00c823f493b4f08d89e4d13efba0d589fc2ecb17
Dom Williams <dxw405@cs.bham.ac.uk>
2017-11-08 19:17:56 +0000

    Add handling framework for specified message types

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -4,32 +4,61 @@ import sys
 from proto import message_pb2
 
 
-def _exit(msg):
-    sys.stderr.write(msg + "\n")
-    sys.exit(1)
+def _null_handler(_self, _msg):
+    pass
 
 
-def read_messages(f):
-    from google.protobuf.internal.decoder import _DecodeVarint32 as decoder
+class Processor:
+    HANDLERS = {}  # initialised after class definition
 
-    while True:
-        len_raw = f.peek()
-        if not len_raw:
-            break
+    def __init__(self, thread_id):
+        self.thread_id = thread_id
+        self.callstack = []
 
-        len_int, to_consume = decoder(len_raw, 0)
-        f.read(to_consume)
+    def handle_message(self, msg):
+        payload = getattr(msg, msg.WhichOneof("payload"))
+        self.HANDLERS[msg.type](self, payload)
 
-        msg_raw = f.read(len_int)
-        if not msg_raw:
-            break
+    def handle_method_enter(self, msg):
+        self.callstack.append(f"{getattr(msg, 'class')}:{msg.method}")
+        print(f"{self.thread_id} >>> {self.callstack[-1]}")
 
-        msg = message_pb2.Variant()
-        msg.ParseFromString(msg_raw)
-        yield msg
+    def handle_method_exit(self, _msg):
+        print(f"{self.thread_id} <<< {self.callstack[-1]}")
+        self.callstack.pop()
+
+
+Processor.HANDLERS.update({val: getattr(Processor, f"handle_{key.lower()}", _null_handler)
+                           for (key, val) in message_pb2.MessageType.items()})
+
+# thread id -> processor
+processors = {}
 
 
 def main():
+    def _exit(msg):
+        sys.stderr.write(msg + "\n")
+        sys.exit(1)
+
+    def read_messages(f):
+        from google.protobuf.internal.decoder import _DecodeVarint32 as decoder
+
+        while True:
+            len_raw = f.peek()
+            if not len_raw:
+                break
+
+            len_int, to_consume = decoder(len_raw, 0)
+            f.read(to_consume)
+
+            msg_raw = f.read(len_int)
+            if not msg_raw:
+                break
+
+            msg = message_pb2.Variant()
+            msg.ParseFromString(msg_raw)
+            yield msg
+
     try:
         in_file = sys.argv[1]
     except IndexError:
@@ -38,8 +67,12 @@ def main():
     try:
         with open(in_file, "rb") as f:
             for msg in read_messages(f):
-                msg_type = message_pb2.MessageType.Name(msg.type)
-                print(f"Read {msg_type}")
+                try:
+                    proc = processors[msg.threadId]
+                except KeyError:
+                    proc = processors[msg.threadId] = Processor(msg.threadId)
+
+                proc.handle_message(msg)
 
     except OSError as e:
         return _exit("Bad input file: " + e.strerror)
