c303b1883fb0c36cbe92b3d7053e6a08d32e5640
Dom Williams <dxw405@cs.bham.ac.uk>
2017-11-08 16:24:17 +0000

    Add preprocessor skeleton

---
+++ preprocessor/preprocessor.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3.6
+import sys
+
+
+def _exit(msg):
+    sys.stderr.write(msg + "\n")
+    sys.exit(1)
+
+
+def read_messages(f):
+    # TODO read protobuf messages
+    return
+    yield
+
+
+def main():
+    try:
+        in_file = sys.argv[1]
+    except IndexError:
+        return _exit(f"Usage: {sys.argv[0]} <monitor output log>\n")
+
+    try:
+        with open(in_file, "rb") as f:
+            for msg in read_messages(f):
+                # TODO use msg
+                print(f"Read {msg}")
+
+    except OSError as e:
+        return _exit("Bad input file: " + e.strerror)
+
+
+if __name__ == '__main__':
+    main()
