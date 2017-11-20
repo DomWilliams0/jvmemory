eed816f42df7073119aea6a85d9b467e0addbc55
Dom Williams <dxw405@cs.bham.ac.uk>
2017-11-20 17:14:17 +0000

    Fix enum final finalize bug

--- monitor-agent/src/main/java/ms/domwillia/jvmemory/SpecimenRunner.java
+++ monitor-agent/src/main/java/ms/domwillia/jvmemory/SpecimenRunner.java
@@ -1,11 +1,10 @@
 package ms.domwillia.jvmemory;
 
-import ms.domwillia.jvmemory.specimen.Threads;
+import ms.domwillia.jvmemory.specimen.Enums;
 
 public class SpecimenRunner {
 
 	public static void main(String[] args) {
-		new Threads().go();
-
+		new Enums().go();
 	}
 }
