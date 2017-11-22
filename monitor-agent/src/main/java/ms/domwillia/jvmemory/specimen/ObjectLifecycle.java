7c482b0c80c19e777f004dbc7f1814e7bfd8561e
Dom Williams <dxw405@cs.bham.ac.uk>
2017-11-22 10:45:01 +0000

    Add extra specimens

--- monitor-agent/src/main/java/ms/domwillia/jvmemory/specimen/ObjectLifecycle.java
+++ monitor-agent/src/main/java/ms/domwillia/jvmemory/specimen/ObjectLifecycle.java
@@ -7,7 +7,7 @@ public class ObjectLifecycle implements Specimen {
 	@Override
 	public void go() {
 
-		for (int i = 0; i < 5000; i++) {
+		for (int i = 0; i < 20; i++) {
 			new EmptyChildBean.InnerBean();
 		}
 	}
