4765166d998fc2a05bc6b9dc090034344cfee324
Dom Williams <dxw405@cs.bham.ac.uk>
2017-12-04 21:43:13 +0000

    Add callgraph history endpoint

--- preprocessor/vis_server.py
+++ preprocessor/vis_server.py
@@ -6,6 +6,31 @@ PORT = 48771
 HOST = "localhost"
 
 
+class BaseHandler(web.RequestHandler):
+    def data_received(self, chunk):
+        pass
+
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header('Content-Type', 'application/json')
+
+
+class CallgraphHandler(BaseHandler):
+    def initialize(self, graph=None):
+        self.graph = graph
+
+    def get(self):
+        self.write(self.graph)
+
+
+class PlaybackHandler(BaseHandler):
+    def initialize(self, history=None):
+        self.history = history
+
+    def get(self):
+        self.write(self.history)
+
+
 def serve_callgraph(processors):
     def convert_to_cytoscapejs(graph):
         cytoscaped = []
@@ -23,20 +48,20 @@ def serve_callgraph(processors):
                           } for ((u, v), data) in graph.edges.items())
         return cytoscaped
 
+    def generate_history(graph):
+        return [
+            [func, data["count"]] for ((_, func), data) in graph.edges.items()
+        ]
+
     # TODO only care about main thread for now
     proc = processors[0]
 
     graph = json.dumps(convert_to_cytoscapejs(proc.graph))
-
-    class CallgraphHandler(web.RequestHandler):
-        def get(self):
-            self.write(graph)
-
-        def set_default_headers(self):
-            self.set_header("Access-Control-Allow-Origin", "*")
+    history = json.dumps(generate_history(proc.graph))
 
     app = web.Application([
-        (r"/callgraph", CallgraphHandler)
+        (r"/callgraph", CallgraphHandler, {"graph": graph}),
+        (r"/playback", PlaybackHandler, {"history": history})
     ])
     app.listen(PORT, address=HOST)
     print(f"Listening on {HOST}:{PORT}")
