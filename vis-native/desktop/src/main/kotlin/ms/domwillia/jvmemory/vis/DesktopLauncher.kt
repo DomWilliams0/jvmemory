28b99473a7b36a1caa8ee86e62247859ed97ac32
Dom Williams <dxw405@cs.bham.ac.uk>
2017-12-28 15:44:03 +0000

    Add default libgdx project

---
+++ vis-native/desktop/src/main/kotlin/ms/domwillia/jvmemory/vis/DesktopLauncher.kt
@@ -0,0 +1,12 @@
+package ms.domwillia.jvmemory.vis
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(JVMemoryVisualisation(), config)
+    }
+}
