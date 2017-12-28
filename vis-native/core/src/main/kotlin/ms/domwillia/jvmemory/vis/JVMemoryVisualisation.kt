b007d721db00b16f6958fde249f8710a7ea0fdad
Dom Williams <dxw405@cs.bham.ac.uk>
2017-12-28 17:56:19 +0000

    Kotlinise example project

--- vis-native/core/src/main/kotlin/ms/domwillia/jvmemory/vis/JVMemoryVisualisation.kt
+++ vis-native/core/src/main/kotlin/ms/domwillia/jvmemory/vis/JVMemoryVisualisation.kt
@@ -1,30 +1,14 @@
 package ms.domwillia.jvmemory.vis
 
-import com.badlogic.gdx.ApplicationAdapter
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import ktx.app.KtxGame
+import ktx.app.KtxScreen
 
-class JVMemoryVisualisation : ApplicationAdapter() {
-    private lateinit var batch: SpriteBatch
-    private lateinit var img: Texture
+class JVMemoryVisualisation : KtxGame<KtxScreen>() {
 
     override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        addScreen(TestScreen())
+        setScreen<TestScreen>()
     }
 
-    override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
-    }
 
-    override fun dispose() {
-        batch.dispose()
-        img.dispose()
-    }
 }
---
+++ vis-native/core/src/main/kotlin/ms/domwillia/jvmemory/vis/TestScreen.kt
@@ -0,0 +1,29 @@
+package ms.domwillia.jvmemory.vis
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import ktx.app.KtxScreen
+import ktx.app.use
+
+class TestScreen : KtxScreen {
+    private val batch: SpriteBatch = SpriteBatch()
+    private val img: Texture = Texture("badlogic.jpg")
+    
+    override fun render(delta: Float) {
+        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        batch.use {
+            it.draw(img, 0f, 0f)
+        }
+    }
+
+    override fun dispose() {
+        batch.dispose()
+        img.dispose()
+    }
+
+
+}
