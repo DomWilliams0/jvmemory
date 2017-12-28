8ab979743d56e5eee4de4ef1d97f52ab3c092d2d
Dom Williams <dxw405@cs.bham.ac.uk>
2017-12-28 20:12:52 +0000

    Remove default render test

--- vis-native/core/src/main/kotlin/ms/domwillia/jvmemory/visualisation/TestScreen.kt
+++ vis-native/core/src/main/kotlin/ms/domwillia/jvmemory/visualisation/TestScreen.kt
@@ -2,33 +2,16 @@ package ms.domwillia.jvmemory.visualisation
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import ktx.app.KtxScreen
-import ktx.app.use
 import ms.domwillia.jvmemory.parser.parseLog
 
 class TestScreen : KtxScreen {
-    private val batch: SpriteBatch = SpriteBatch()
-    private val img: Texture = Texture("badlogic.jpg")
-
     init {
         parseLog("../../../monitor-agent/jvmemory.log")
     }
 
     override fun render(delta: Float) {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(0.15f, 0.15f, 0.19f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-
-        batch.use {
-            it.draw(img, 0f, 0f)
-        }
     }
-
-    override fun dispose() {
-        batch.dispose()
-        img.dispose()
-    }
-
-
 }
