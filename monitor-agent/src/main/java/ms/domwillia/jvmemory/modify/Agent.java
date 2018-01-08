b462005e5abb537e53dba48dbb13a997534cdc59
Dom Williams <dxw405@cs.bham.ac.uk>
2018-01-08 14:55:24 +0000

    Amend instrumentation class name check to be even more awful

--- monitor-agent/src/main/java/ms/domwillia/jvmemory/modify/Agent.java
+++ monitor-agent/src/main/java/ms/domwillia/jvmemory/modify/Agent.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
@@ -18,15 +17,17 @@ public class Agent implements ClassFileTransformer {
 
 	public byte[] transform(final ClassLoader loader, String className,
 	                        final Class classBeingRedefined, final ProtectionDomain protectionDomain,
-	                        final byte[] classfileBuffer) throws IllegalClassFormatException {
+	                        final byte[] classfileBuffer) {
 
-		if (className.startsWith("ms/domwillia/jvmemory/specimen")) {
+		if (className.startsWith("ms/domwillia") &&
+				(!className.startsWith("ms/domwillia/jvmemory/") ||
+						className.startsWith("ms/domwillia/jvmemory/specimen"))) {
 			ClassReader reader = new ClassReader(classfileBuffer);
 			ClassWriter writer = new ClassWriter(reader, 0);
 			PatchingClassVisitor visitor = new PatchingClassVisitor(writer);
 			try {
 				reader.accept(visitor, ClassReader.EXPAND_FRAMES);
-			} catch(RuntimeException e) {
+			} catch (RuntimeException e) {
 				e.printStackTrace();
 			}
 
