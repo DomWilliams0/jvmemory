dbf177a39f8000abce1b68f744affd6e7dd4cffb
Dom Williams <dxw405@cs.bham.ac.uk>
2018-01-13 19:09:22 +0000

    Add protobuf logging of all events

--- jvmti-agent/jvmti_logger/src/lib.rs
+++ jvmti-agent/jvmti_logger/src/lib.rs
@@ -5,8 +5,10 @@ mod proto;
 
 use libc::*;
 use std::ffi::CStr;
+use proto::*;
+use proto::message::{Variant, MessageType};
 
-type Long = isize;
+type Long = i64;
 type Int = i32;
 type String = *const c_char;
 
@@ -25,49 +27,101 @@ macro_rules! get_string {
                 eprintln!("jni passed bad String as '{}'", stringify!($p));
                 return;
             },
-            Ok(cstr) => cstr,
+            Ok(cstr) => cstr.to_owned(),
         }
     }}
 }
 
+// TODO this is all horribly repetitive and can definitely be replaced with some macro magic
+
 #[no_mangle]
 pub extern fn on_enter_method(class: String, method: String) {
-    println!(">>> {}:{}", get_string!(class), get_string!(method));
-}
+    let mut payload = flow::MethodEnter::new();
+    payload.class = get_string!(class);
+    payload.method = get_string!(method);
+
+    let mut msg = Variant::new();
+    msg.set_method_enter(payload);
+    msg.set_field_type(MessageType::METHOD_ENTER);
+    log_message(msg);}
 
 #[no_mangle]
 pub extern fn on_exit_method() {
-    println!("<<<");
-}
+    let payload = flow::MethodExit::new();
+
+    let mut msg = Variant::new();
+    msg.set_method_exit(payload);
+    msg.set_field_type(MessageType::METHOD_EXIT);
+    log_message(msg);}
 
 #[no_mangle]
 pub extern fn on_get_field(obj_id: Long, field: String) {
-    println!("getfield {} on {}", get_string!(field), obj_id);
-}
+    let mut payload = access::GetField::new();
+    payload.id = obj_id;
+    payload.field = get_string!(field);
+
+    let mut msg = Variant::new();
+    msg.set_get_field(payload);
+    msg.set_field_type(MessageType::GETFIELD);
+    log_message(msg);}
 
 #[no_mangle]
 pub extern fn on_put_field(obj_id: Long, field: String, value_id: Long) {
-    println!("putfield {} on {} with value {}", get_string!(field), obj_id, value_id);
+    let mut payload = access::PutField::new();
+    payload.id = obj_id;
+    payload.field = get_string!(field);
+    payload.value_id = value_id;
+
+    let mut msg = Variant::new();
+    msg.set_put_field(payload);
+    msg.set_field_type(MessageType::PUTFIELD);
+    log_message(msg);
 }
 
 #[no_mangle]
 pub extern fn on_store(value_id: Long, index: Int) {
-    println!("store {} in index {}", value_id, index);
+    let mut payload = access::Store::new();
+    payload.value_id = value_id;
+    payload.index = index;
+
+    let mut msg = Variant::new();
+    msg.set_store(payload);
+    msg.set_field_type(MessageType::STORE);
+    log_message(msg);
 }
 
 #[no_mangle]
 pub extern fn on_load(index: Int) {
-    println!("load index {}",index);
+    let mut payload = access::Load::new();
+    payload.index = index;
+
+    let mut msg = Variant::new();
+    msg.set_load(payload);
+    msg.set_field_type(MessageType::LOAD);
+    log_message(msg);
 }
 
 #[no_mangle]
 pub extern fn on_alloc(obj_id: Long, class: String) {
-    println!("alloc {} of type {}", obj_id, get_string!(class));
+    let mut payload = allocations::Allocation::new();
+    payload.id = obj_id;
+    payload.field_type = get_string!(class);
+
+    let mut msg = Variant::new();
+    msg.set_alloc(payload);
+    msg.set_field_type(MessageType::ALLOC);
+    log_message(msg);
 }
 
 #[no_mangle]
 pub extern fn on_dealloc(obj_id: Long) {
-    println!("dealloc {}", obj_id);
+    let mut payload = allocations::Deallocation::new();
+    payload.id = obj_id;
+
+    let mut msg = Variant::new();
+    msg.set_dealloc(payload);
+    msg.set_field_type(MessageType::DEALLOC);
+    log_message(msg);
 }
 
 #[no_mangle]
@@ -85,5 +139,12 @@ pub extern fn on_define_class(buffer: *const u8, len: Int) {
         Ok(def) => def
     };
 
-    println!("class defIntion: {:?}", def);
+    let mut msg = Variant::new();
+    msg.set_class_def(def);
+    msg.set_field_type(MessageType::CLASS_DEF);
+    log_message(msg)
+}
+
+fn log_message(message: Variant) {
+    eprintln!("logging {:?}", message);
 }
