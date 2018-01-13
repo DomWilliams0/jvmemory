9eef832689e1e1e404129f3242a75bc46919ac29
Dom Williams <dxw405@cs.bham.ac.uk>
2018-01-13 11:14:31 +0000

    Tidy up native agent

--- jvmti-agent/agent.c
+++ jvmti-agent/agent.c
@@ -2,7 +2,6 @@
 #include <jni.h>
 #include <jvmti.h>
 
-#include "exports.h"
 #include "array.h"
 #include "util.h"
 
@@ -10,9 +9,7 @@
 //		i dont think so
 //		does it matter?
 static JavaVM *jvm = NULL;
-static jvmtiEnv *env = NULL;
-
-static jlong next_id = 1;
+jvmtiEnv *env = NULL;
 
 static struct array freed_objects;
 static jrawMonitorID free_lock;
@@ -27,9 +24,9 @@ static jvmtiError add_capabilities() {
 	return (*env)->AddCapabilities(env, &capa);
 }
 
-static void JNICALL callback_dealloc(jvmtiEnv *jvmti_env, jlong tag);
+static void JNICALL callback_dealloc(jvmtiEnv *env, jlong tag);
 static void JNICALL callback_vm_init(jvmtiEnv *env, JNIEnv *jnienv, jthread thread);
-static void JNICALL callback_gc_finish(jvmtiEnv* env);
+static void JNICALL callback_gc_finish(jvmtiEnv *env);
 
 static jvmtiError register_callbacks() {
 	jvmtiEventCallbacks callbacks = {0};
@@ -115,7 +112,7 @@ static void log_allocation(JNIEnv *jnienv, jlong tag, jclass klass) {
 	}
 }
 
-static void JNICALL free_thread_runnable(jvmtiEnv* env, JNIEnv* jnienv, void *arg) {
+static void JNICALL free_thread_runnable(jvmtiEnv *env, JNIEnv *jnienv, void *arg) {
 	while (1) {
 		DO_SAFE((*env)->RawMonitorEnter(env, free_lock), "entering free monitor");
 
@@ -133,7 +130,7 @@ static void JNICALL free_thread_runnable(jvmtiEnv* env, JNIEnv* jnienv, void *ar
 
 // callbacks
 
-static void JNICALL callback_dealloc(jvmtiEnv *jvmti_env, jlong tag) {
+static void JNICALL callback_dealloc(jvmtiEnv *env, jlong tag) {
 	// no JVMTI or JNI functions can be called in this callback
 	array_add(&freed_objects, tag);
 }
@@ -149,141 +146,8 @@ static void JNICALL callback_vm_init(jvmtiEnv *env, JNIEnv *jnienv, jthread thre
     DO_SAFE((*env)->RunAgentThread(env, new_thread, &free_thread_runnable, NULL, JVMTI_THREAD_MAX_PRIORITY), "creating agent thread");
 }
 
-static void JNICALL callback_gc_finish(jvmtiEnv* env) {
+static void JNICALL callback_gc_finish(jvmtiEnv *env) {
     DO_SAFE((*env)->RawMonitorEnter(env, free_lock), "entering free monitor");
     DO_SAFE((*env)->RawMonitorNotify(env, free_lock), "notifying free monitor");
     DO_SAFE((*env)->RawMonitorExit(env, free_lock), "exiting free monitor");
-}
-
-// exported functions
-
-/*
- * Class:     ms_domwillia_jvmemory_monitor_Monitor
- * Method:    allocateTag
- * Signature: (Ljava/lang/Object;)V
- */
-JNIEXPORT void JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_allocateTag(
-		JNIEnv *jnienv,
-		jclass klass,
-		jobject obj) {
-
-	jlong new_tag = next_id++;
-
-	jvmtiError err;
-	if ((err = (*env)->SetTag(env, obj, new_tag)) == JVMTI_ERROR_NONE) {
-		// debug log
-		char *name = NULL;
-        jclass cls = (*jnienv)->GetObjectClass(jnienv, obj);
-		if ((err = (*env)->GetClassSignature(env, cls, &name, NULL)) == JVMTI_ERROR_NONE) {
-			printf("allocated tag %ld to object of class '%s'\n", new_tag, name);
-			(*env)->Deallocate(env, (unsigned char *)name);
-			name = NULL;
-		} else {
-			fprintf(stderr, "could not get class name: %d\n", err);
-		}
-	} else {
-		fprintf(stderr, "could not allocate tag: %d\n", err);
-	}
-}
-
-/*
- * Class:     ms_domwillia_jvmemory_monitor_Monitor
- * Method:    getTag
- * Signature: (Ljava/lang/Object;)J
- */
-JNIEXPORT jlong JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_getTag(
-		JNIEnv *jnienv,
-		jclass klass,
-		jobject obj) {
-
-	jlong tag = 0L;
-	(*env)->GetTag(env, obj, &tag);
-	return tag;
-}
-
-/*
- * Class:     ms_domwillia_jvmemory_monitor_Monitor
- * Method:    enterMethod
- * Signature: (Ljava/lang/String;Ljava/lang/String;)V
- */
-JNIEXPORT void JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_enterMethod(
-		JNIEnv *jnienv,
-		jclass klass,
-		jstring class_name,
-		jstring method_name) {
-
-	const char *cls = (*jnienv)->GetStringUTFChars(jnienv, class_name, NULL);
-	const char *mthd = (*jnienv)->GetStringUTFChars(jnienv, method_name, NULL);
-    printf(">>> %s:%s\n", cls, mthd);
-    (*jnienv)->ReleaseStringUTFChars(jnienv, class_name, cls);
-	(*jnienv)->ReleaseStringUTFChars(jnienv, method_name, mthd);
-}
-
-/*
- * Class:     ms_domwillia_jvmemory_monitor_Monitor
- * Method:    exitMethod
- * Signature: ()V
- */
-JNIEXPORT void JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_exitMethod(
-		JNIEnv *jnienv,
-		jclass klass) {
-    printf("<<<\n");
-}
-
-// TODO onAlloc and onDealloc
-
-/*
- * Class:     ms_domwillia_jvmemory_monitor_Monitor
- * Method:    onGetField
- * Signature: (JLjava/lang/String;)V
- */
-JNIEXPORT void JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_onGetField(
-		JNIEnv *jnienv,
-		jclass klass,
-		jlong obj_id,
-		jstring field) {
-	const char *field_str = (*jnienv)->GetStringUTFChars(jnienv, field, NULL);
-	printf("getfield %s on %ld\n", field_str, obj_id);
-	(*jnienv)->ReleaseStringUTFChars(jnienv, field, field_str);
-}
-
-/*
- * Class:     ms_domwillia_jvmemory_monitor_Monitor
- * Method:    onPutField
- * Signature: (JLjava/lang/String;J)V
- */
-JNIEXPORT void JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_onPutField(
-		JNIEnv *jnienv,
-		jclass klass,
-		jlong obj_id,
-		jstring field,
-		jlong value_id) {
-	const char *field_str = (*jnienv)->GetStringUTFChars(jnienv, field, NULL);
-	printf("putfield %s on %ld with value %ld\n", field_str, obj_id, value_id);
-	(*jnienv)->ReleaseStringUTFChars(jnienv, field, field_str);
-}
-
-/*
- * Class:     ms_domwillia_jvmemory_monitor_Monitor
- * Method:    onStoreLocalVar
- * Signature: (JI)V
- */
-JNIEXPORT void JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_onStoreLocalVar(
-		JNIEnv *jnienv,
-		jclass klass,
-		jlong value_id,
-		jint index) {
-	printf("store %ld in local var %d\n", value_id, index);
-}
-
-/*
- * Class:     ms_domwillia_jvmemory_monitor_Monitor
- * Method:    onLoadLocalVar
- * Signature: (I)V
- */
-JNIEXPORT void JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_onLoadLocalVar(
-		JNIEnv *jnienv,
-		jclass klass,
-		jint index) {
-	printf("load %d\n", index);
 }
---
+++ jvmti-agent/exports.c
@@ -0,0 +1,137 @@
+#include <jni.h>
+#include <jvmti.h>
+#include "agent.h"
+#include "exports.h"
+
+static jlong next_id = 1;
+
+/*
+ * Class:     ms_domwillia_jvmemory_monitor_Monitor
+ * Method:    allocateTag
+ * Signature: (Ljava/lang/Object;)V
+ */
+JNIEXPORT void JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_allocateTag(
+        JNIEnv *jnienv,
+        jclass klass,
+        jobject obj) {
+
+    jlong new_tag = next_id++;
+
+    jvmtiError err;
+    if ((err = (*env)->SetTag(env, obj, new_tag)) == JVMTI_ERROR_NONE) {
+        // debug log
+        char *name = NULL;
+        jclass cls = (*jnienv)->GetObjectClass(jnienv, obj);
+        if ((err = (*env)->GetClassSignature(env, cls, &name, NULL)) == JVMTI_ERROR_NONE) {
+            printf("allocated tag %ld to object of class '%s'\n", new_tag, name);
+            (*env)->Deallocate(env, (unsigned char *)name);
+            name = NULL;
+        } else {
+            fprintf(stderr, "could not get class name: %d\n", err);
+        }
+    } else {
+        fprintf(stderr, "could not allocate tag: %d\n", err);
+    }
+}
+
+/*
+ * Class:     ms_domwillia_jvmemory_monitor_Monitor
+ * Method:    getTag
+ * Signature: (Ljava/lang/Object;)J
+ */
+JNIEXPORT jlong JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_getTag(
+        JNIEnv *jnienv,
+        jclass klass,
+        jobject obj) {
+
+    jlong tag = 0L;
+    (*env)->GetTag(env, obj, &tag);
+    return tag;
+}
+
+/*
+ * Class:     ms_domwillia_jvmemory_monitor_Monitor
+ * Method:    enterMethod
+ * Signature: (Ljava/lang/String;Ljava/lang/String;)V
+ */
+JNIEXPORT void JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_enterMethod(
+        JNIEnv *jnienv,
+        jclass klass,
+        jstring class_name,
+        jstring method_name) {
+
+    const char *cls = (*jnienv)->GetStringUTFChars(jnienv, class_name, NULL);
+    const char *mthd = (*jnienv)->GetStringUTFChars(jnienv, method_name, NULL);
+    printf(">>> %s:%s\n", cls, mthd);
+    (*jnienv)->ReleaseStringUTFChars(jnienv, class_name, cls);
+    (*jnienv)->ReleaseStringUTFChars(jnienv, method_name, mthd);
+}
+
+/*
+ * Class:     ms_domwillia_jvmemory_monitor_Monitor
+ * Method:    exitMethod
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_exitMethod(
+        JNIEnv *jnienv,
+        jclass klass) {
+    printf("<<<\n");
+}
+
+// TODO onAlloc and onDealloc
+
+/*
+ * Class:     ms_domwillia_jvmemory_monitor_Monitor
+ * Method:    onGetField
+ * Signature: (JLjava/lang/String;)V
+ */
+JNIEXPORT void JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_onGetField(
+        JNIEnv *jnienv,
+        jclass klass,
+        jlong obj_id,
+        jstring field) {
+    const char *field_str = (*jnienv)->GetStringUTFChars(jnienv, field, NULL);
+    printf("getfield %s on %ld\n", field_str, obj_id);
+    (*jnienv)->ReleaseStringUTFChars(jnienv, field, field_str);
+}
+
+/*
+ * Class:     ms_domwillia_jvmemory_monitor_Monitor
+ * Method:    onPutField
+ * Signature: (JLjava/lang/String;J)V
+ */
+JNIEXPORT void JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_onPutField(
+        JNIEnv *jnienv,
+        jclass klass,
+        jlong obj_id,
+        jstring field,
+        jlong value_id) {
+    const char *field_str = (*jnienv)->GetStringUTFChars(jnienv, field, NULL);
+    printf("putfield %s on %ld with value %ld\n", field_str, obj_id, value_id);
+    (*jnienv)->ReleaseStringUTFChars(jnienv, field, field_str);
+}
+
+/*
+ * Class:     ms_domwillia_jvmemory_monitor_Monitor
+ * Method:    onStoreLocalVar
+ * Signature: (JI)V
+ */
+JNIEXPORT void JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_onStoreLocalVar(
+        JNIEnv *jnienv,
+        jclass klass,
+        jlong value_id,
+        jint index) {
+    printf("store %ld in local var %d\n", value_id, index);
+}
+
+/*
+ * Class:     ms_domwillia_jvmemory_monitor_Monitor
+ * Method:    onLoadLocalVar
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL Java_ms_domwillia_jvmemory_monitor_Monitor_onLoadLocalVar(
+        JNIEnv *jnienv,
+        jclass klass,
+        jint index) {
+    printf("load %d\n", index);
+}
