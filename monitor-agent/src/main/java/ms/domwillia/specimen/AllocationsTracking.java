50f94e5e9e95f9d92eee62bf35c81ab4ea877735
Dom Williams <dxw405@cs.bham.ac.uk>
2018-01-14 15:15:43 +0000

    Add heap graph rendering test

--- monitor-agent/src/main/java/ms/domwillia/specimen/AllocationsTracking.java
+++ monitor-agent/src/main/java/ms/domwillia/specimen/AllocationsTracking.java
@@ -1,55 +1,24 @@
 package ms.domwillia.specimen;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class AllocationsTracking implements Specimen {
 
 	@Override
 	public void go() {
-		Baggage baggageA = new Baggage("rather heavy", 100);
-		Baggage baggageB = new Baggage("quite light", 20);
-
-		TestBean bean = new TestBean(20, 5.5, baggageA);
-		Baggage tmp = bean.baggage;
-		bean.baggage = baggageB;
-
-		List<Baggage> baggages = new ArrayList<>();
-		baggages.add(new Baggage("first", 5));
-		baggages.add(new Baggage("second", 10));
-
-		for (Baggage b : baggages) {
-			bean.baggage = b;
-		}
-		bean.baggage = baggageA;
-	}
-
-	private class TestBean {
-		int anInt;
-		double aDouble;
-		Baggage baggage;
-
-		public TestBean(int anInt, double aDouble, Baggage baggage) {
-			this.anInt = anInt;
-			this.aDouble = aDouble;
-			this.baggage = baggage;
+		Link last = new Link("initial", null);
+		for (int i = 1; i <= 5; i++) {
+			Link l = new Link(Integer.toString(i), null);
+			last.next = l;
+			last = l;
 		}
 	}
 
-	private class Baggage extends BaggageParent {
-		int anInt;
-
-		public Baggage(String name, int anInt) {
-			super(name);
-			this.anInt = anInt;
-		}
-	}
-
-	private class BaggageParent {
+	private class Link {
 		String name;
+		Link next;
 
-		public BaggageParent(String name) {
+		Link(String name, Link next) {
 			this.name = name;
+			this.next = next;
 		}
 	}
 }
--- vis-native/preprocessing/src/main/kotlin/ms/domwillia/jvmemory/preprocessing/GraphProcessor.kt
+++ vis-native/preprocessing/src/main/kotlin/ms/domwillia/jvmemory/preprocessing/GraphProcessor.kt
@@ -1,13 +1,19 @@
 package ms.domwillia.jvmemory.preprocessing
 
 import edu.uci.ics.jung.graph.DirectedSparseGraph
+import edu.uci.ics.jung.graph.ObservableGraph
 import edu.uci.ics.jung.graph.util.EdgeType
+import edu.uci.ics.jung.visualization.BasicVisualizationServer
 import ms.domwillia.jvmemory.protobuf.Access
 import ms.domwillia.jvmemory.protobuf.Allocations
+import java.awt.Color
+import java.awt.Dimension
+import javax.swing.JFrame
+import javax.swing.SwingUtilities
 
 class GraphProcessor(threadId: Long) : Processor(threadId) {
 
-    data class Edge(val field: String)
+    data class Edge(val field: String, val from: Long)
 
     private val graph = DirectedSparseGraph<Long, Edge>()
     private val nodeTypes = mutableMapOf<Long, String>()
@@ -29,15 +35,42 @@ class GraphProcessor(threadId: Long) : Processor(threadId) {
         }
 
         graph.addEdge(
-                Edge(message.field),
+                Edge(message.field, message.id),
                 message.id,
                 message.valueId,
                 EdgeType.DIRECTED
         )
-        println("Adding new ${message.id} ${message.field} to ${message.valueId}")
+        println("Adding edge: ${message.id}.${message.field} = ${message.valueId}")
     }
 
     override fun finish() {
         // TODO render graph
+
+        val layout = edu.uci.ics.jung.algorithms.layout.ISOMLayout(graph)
+        val vis = BasicVisualizationServer(
+                layout,
+                Dimension(600, 600)
+        )
+        vis.renderContext.setVertexLabelTransformer {
+            val type = run {
+                val ty = nodeTypes[it]!!
+                ty.substring(ty.lastIndexOf('/') + 1, ty.length - 1)
+            }
+            "$type:$it"
+        }
+        vis.renderContext.setEdgeLabelTransformer { it?.field }
+        vis.renderContext.setVertexFillPaintTransformer {
+            when (nodeTypes[it]!!) {
+                "Lms/domwillia/specimen/AllocationsTracking;" -> Color.GREEN
+                "Lms/domwillia/specimen/AllocationsTracking\$Link;" -> Color.BLUE
+                else -> {Color.RED}
+            }
+        }
+
+        val frame = JFrame()
+        frame.size = Dimension(600, 600)
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        frame.add(vis)
+        frame.isVisible = true
     }
 }
