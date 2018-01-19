c62368f4d59b05ab63936902f8500334c1a9e714
Dom Williams <dxw405@cs.bham.ac.uk>
2018-01-19 13:50:17 +0000

    Migrate heap graph to javafx

--- visualisation/src/main/kotlin/ms/domwillia/jvmemory/visualisation/VisualisationRunner.kt
+++ visualisation/src/main/kotlin/ms/domwillia/jvmemory/visualisation/VisualisationRunner.kt
@@ -1,16 +1,14 @@
 package ms.domwillia.jvmemory.visualisation
 
-import ms.domwillia.jvmemory.preprocessor.Preprocessor
-import java.io.File
+import javafx.application.Application
 
 object VisualisationRunner {
     @JvmStatic
     fun main(args: Array<String>) {
-        val events = Preprocessor.runPreprocessor(
-                File("../monitor-agent/jvmemory.log"),
-                File("target/vis-events")
+        Application.launch(
+                Visualisation::class.java,
+                "../monitor-agent/jvmemory.log",
+                "target/vis-events"
         )
-        val size = Pair(600, 600)
-        Visualisation(size, events)
     }
 }
