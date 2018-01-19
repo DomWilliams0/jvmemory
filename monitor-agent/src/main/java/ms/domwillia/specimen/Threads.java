a7c0275de6d43f3dbcf14a6c748eec930f8147cf
Dom Williams <dxw405@cs.bham.ac.uk>
2018-01-19 13:11:09 +0000

    Add wasteful thread specimen

--- monitor-agent/src/main/java/ms/domwillia/specimen/Threads.java
+++ monitor-agent/src/main/java/ms/domwillia/specimen/Threads.java
@@ -1,31 +1,34 @@
 package ms.domwillia.specimen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Threads implements Specimen {
 
-	private static void staticFunction() {
-		otherStaticFunction();
-	}
-
-	private static void otherStaticFunction() {
-
+	private static void wasteMemory() {
+		for (int i = 0; i < 1000; i++) {
+			new GarbageCollection.WasteOfSpace(String.valueOf(i));
+		}
 	}
 
 	@Override
 	public void go() {
-		Thread[] ts = new Thread[8];
-		for (int i = 0; i < ts.length; i++) {
-			ts[i] = new Thread(Threads::staticFunction);
+		int count = 20;
+		List<Thread> ts = new ArrayList<>(count);
+		for (int i = 0; i < count; i++) {
+			ts.add(new Thread(Threads::wasteMemory));
 		}
 
 		for (Thread t : ts)
 			t.start();
 
-		for (Thread t : ts)
+		for (int i = 0; i < ts.size(); i++) {
+			Thread t = ts.get(i);
 			try {
 				t.join();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-
+		}
 	}
 }
