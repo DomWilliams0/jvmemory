e62f41f3f4d300db241ffc9c68ee55b3ccef0d99
Dom Williams <dxw405@cs.bham.ac.uk>
2018-01-23 14:11:13 +0000

    Add buildJar target for vis server

--- visualisation/run.sh
+++ visualisation/run.sh
@@ -3,10 +3,10 @@
 echo Generating proto js...
 ./gen_proto.sh
 
-
 #echo Running preprocessor...
 #(cd ../preprocessor && ./gradlew run)
 # use `-Pcached` when running vis server to preprocess separately
 
 echo Starting visualisation server...
-(cd ../visualisation-server && ./gradlew run)
+(cd ../visualisation-server && ./gradlew buildJar)
+java -jar ../visualisation-server/build/libs/visualisation-server-0.1.jar
