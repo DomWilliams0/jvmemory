5a2d988455b26c093403e2a435efab7663b05c3b
Dom Williams <dxw405@cs.bham.ac.uk>
2018-01-29 10:05:14 +0000

    Add optional xdg-open-ality

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,10 +3,15 @@ DIR=jvmemory-all
 MAIN=$1
 PACKAGES=$2
 
-if [[ $# != 2 ]]; then
-	echo "Usage: $0 <main class> <comma separated list of packages to instrument>"
+if [[ $# != 2 && $# != 3 ]]; then
+	echo "Usage: $0 <main class> <comma separated list of packages to instrument> [open]"
 	exit 1
 fi
+OPEN=0
+if [[ $3 = "open" ]]; then
+	OPEN=1
+fi
+
 
 i=0
 while true; do
@@ -22,11 +27,13 @@ java \
 	-agentpath:$DIR/libagent.so=$PROJ/jvmemory.log \
 	$MAIN
 
-(
-echo Opening visualisation in browser in 1 second
-sleep 1
-xdg-open $DIR/vis-src/vis.html
-) &
+if [[ $OPEN = 1 ]]; then
+	(
+	echo Opening visualisation in browser in 1 second
+	sleep 1
+	xdg-open $DIR/vis-src/vis.html
+	) &
+fi
 
 echo Starting visualisation server
 java -jar $DIR/server.jar $PROJ/jvmemory.log $PROJ/vis-events $DIR/vis-src
