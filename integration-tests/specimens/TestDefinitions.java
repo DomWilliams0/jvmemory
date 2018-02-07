53de0acba578d42f10f883dbee2b928397838086
Dom Williams <dxw405@cs.bham.ac.uk>
2018-02-07 17:58:07 +0000

    Add functionality to run jvmemory against a specimen

---
+++ integration-tests/specimens/TestDefinitions.java
@@ -0,0 +1,28 @@
+package specimens;
+
+class TestDefinitions {
+
+	private static int anInt;
+	private String aString;
+	public Object anObject;
+
+	private void a() {
+
+	}
+
+	private int b(int x) {
+		return x;
+	}
+
+	private String c(String a, Long b, int[][] c) {
+		return null;
+	}
+
+	public static void d() {
+
+	}
+
+	public static void main(String[] args) {
+		// for completeness
+	}
+}
