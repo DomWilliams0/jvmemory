18ac2826d8623bbc64a9fd028cb5e08530819850
Dom Williams <dxw405@cs.bham.ac.uk>
2018-02-07 18:33:22 +0000

    Add protobuf message parsing

--- integration-tests/test_monitor.py
+++ integration-tests/test_monitor.py
@@ -1,25 +1,26 @@
-import getpass
+import io
 import os
 import shutil
 import subprocess
 import sys
-import tempfile
+import typing
 import unittest
 from pathlib import Path
 
+from pb import message_pb2
 
-def _get_env(name: str) -> str:
+
+def _get_env(name: str) -> Path:
     var = os.getenv(name)
     if not var:
         print(f"Missing env var ${name}")
         sys.exit(1)
-    return var
+    return Path(var)
 
 
-INSTALL_DIR: Path = Path(_get_env("JVMEMORY_DIR"))
-TESTS_DIR: Path = Path(_get_env("JVMEMORY_TEST_DIR"))
-
-WORKING_DIR: Path = Path(tempfile.gettempdir()) / f"jvmemory-tests-{getpass.getuser()}"
+INSTALL_DIR: Path = _get_env("JVMEMORY_DIR")
+TESTS_DIR: Path = _get_env("JVMEMORY_TEST_DIR")
+WORKING_DIR: Path = _get_env("JVMEMORY_WORKING_DIR")
 SPECIMEN_DIR: Path = Path(TESTS_DIR) / "specimens"
 
 
@@ -58,17 +59,42 @@ def run_specimen(specimen_name: str, out_file: Path):
     )
 
 
+def read_messages(f: typing.BinaryIO) -> typing.Generator[message_pb2.Variant, None, None]:
+    from google.protobuf.internal.decoder import _DecodeVarint32 as decoder
+
+    while True:
+        len_raw = f.peek()
+        if not len_raw:
+            break
+
+        len_int, to_consume = decoder(len_raw, 0)
+        f.read(to_consume)
+
+        msg_raw = f.read(len_int)
+        if not msg_raw:
+            break
+
+        msg = message_pb2.Variant()
+        msg.ParseFromString(msg_raw)
+        yield msg
+
+
 class MonitorTest(unittest.TestCase):
-    LOG_PATH = WORKING_DIR / "definitions.log"
+    LOG_PATH: Path = WORKING_DIR / "definitions.log"
+    MESSAGES: typing.List[message_pb2.Variant]
 
     @classmethod
     def setUpClass(cls):
-        run_specimen("TestDefinitions", MonitorTest.LOG_PATH)
-        if not MonitorTest.LOG_PATH.exists():
+        run_specimen("TestDefinitions", cls.LOG_PATH)
+        if not cls.LOG_PATH.exists():
             raise RuntimeError("Specimen didn't run")
+
+        with io.open(str(cls.LOG_PATH), 'rb') as f:
+            cls.MESSAGES = list(read_messages(f))
 
     @classmethod
     def tearDownClass(cls):
+        cls.MESSAGES.clear()
         _delete_working_dir()
 
     def test_test(self):
