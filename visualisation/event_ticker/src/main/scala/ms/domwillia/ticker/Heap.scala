d4cd7f428f1c2b911826f23b24bddac9e2bced91
Dom Williams <dxw405@cs.bham.ac.uk>
2018-02-14 19:50:35 +0000

    Add intra heap links

--- visualisation/event_ticker/src/main/scala/ms/domwillia/ticker/Heap.scala
+++ visualisation/event_ticker/src/main/scala/ms/domwillia/ticker/Heap.scala
@@ -1,25 +1,8 @@
 package ms.domwillia.ticker
 
 import scala.scalajs.js
-import scala.scalajs.js.{Dynamic, UndefOr}
+import scala.scalajs.js.UndefOr
 import scala.scalajs.js.annotation.JSExportTopLevel
-
-/*@js.native
-trait Node extends js.Object {
-  val id: Long = js.native
-  val clazz: String = js.native
-  val array: js.Dynamic = js.native
-  val fill: String = js.native
-  val x: Double = js.native
-  val y: Double = js.native
-}
-
-@js.native
-trait Link extends js.Object {
-  val source: Long = js.native
-  val target: Long = js.native
-  val name: String = js.native
-}*/
 
 object ArrayMeta {
   def apply(size: Int, `type`: String): Option[ArrayMeta] = size match {
@@ -39,6 +22,4 @@ class Node(val id: Long, val clazz: String, val array: UndefOr[ArrayMeta], val s
 // TODO stackmeta
 
 @JSExportTopLevel("Link")
-class Link extends js.Object {
-
-}
+class Link(val source: Node, var target: Node, val name: String) extends js.Object
