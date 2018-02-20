7bc5bb8c6b785ead39b2e9538de80ad6d1266670
Dom Williams <dxw405@cs.bham.ac.uk>
2018-02-20 22:04:08 +0000

    Extract field functions to separate unit

--- jvmti-agent/fields.h
+++ jvmti-agent/fields.h
@@ -11,16 +11,31 @@ typedef struct field *fields_p;
 
 extern fields_map_p fields_init();
 
-extern fields_p fields_get(fields_map_p map, const char *cls, int *count);
+extern fields_p fields_get(fields_map_p map,
+                           const char *cls,
+                           int *count);
 
 extern void fields_free(fields_map_p map);
 
 extern fields_discovery_p fields_discovery_init();
 
-extern int fields_discovery_check(fields_discovery_p discover, const char *cls);
+extern int fields_discovery_check(fields_discovery_p discover,
+                                  const char *cls);
 
-extern void fields_discovery_register(fields_discovery_p discover, const char *name, const char *cls);
+extern void fields_discovery_register(fields_discovery_p discover,
+                                      const char *name,
+                                      const char *cls);
 
-extern void fields_discovery_finish(fields_discovery_p discover, fields_map_p map, const char *cls);
+extern void fields_discovery_finish(fields_discovery_p discover,
+                                    fields_map_p map,
+                                    const char *cls);
+
+void discover_all_fields(JNIEnv *jnienv,
+                         jclass cls,
+                         fields_discovery_p discover);
+
+void follow_references(jobject obj,
+                       fields_p fields,
+                       int count);
 
 #endif
