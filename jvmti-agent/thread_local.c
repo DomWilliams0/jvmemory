adb06a506250ca08b6d812ab761d0992cdae18f2
Dom Williams <dxw405@cs.bham.ac.uk>
2018-02-25 13:04:41 +0000

    Add thread-local storage allocation

---
+++ jvmti-agent/thread_local.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+#include "thread_local.h"
+#include "util.h"
+
+void thread_local_state_init()
+{
+	struct thread_local_state *state = calloc(1, sizeof(struct thread_local_state));
+	DO_SAFE_COND(state != NULL, "allocating thread local state");
+
+	DO_SAFE((*env)->SetThreadLocalStorage(env, NULL, state), "set thread local storage");
+
+}
+
+void thread_local_state_free()
+{
+	struct thread_local_state *state = thread_local_state_get();
+	free(state);
+	DO_SAFE((*env)->SetThreadLocalStorage(env, NULL, NULL), "set thread local storage");
+}
+
+// will exit on failure (DO_SAFE) so never returns null
+// error "handling"!
+struct thread_local_state *thread_local_state_get()
+{
+	void* state = NULL;
+	DO_SAFE((*env)->GetThreadLocalStorage(env, NULL, &state), "get thread local storage");
+	return state;
+}
