379f8ef2f1b56a69617781f276e20b13cfc1566a
Dom Williams <dxw405@cs.bham.ac.uk>
2018-03-14 17:16:00 +0000

    Remove answered question comment

--- monitor-agent/src/main/kotlin/ms/domwillia/jvmemory/modify/patcher/MethodPatcher.kt
+++ monitor-agent/src/main/kotlin/ms/domwillia/jvmemory/modify/patcher/MethodPatcher.kt
@@ -319,7 +319,6 @@ class MethodPatcher(
     }
 
     override fun visitLocalVariable(name: String, desc: String, signature: String?, start: Label?, end: Label?, index: Int) {
-        // TODO what if no debugging symbols? is desc null
         definition.registerLocalVariable(name, desc, index)
         super.visitLocalVariable(name, desc, signature, start, end, index)
     }
